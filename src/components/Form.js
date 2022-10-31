c6c1ad344d0b88e0bf7285c05b0306c3381dce55
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-10-31 10:45:41 -0600

    fix compiling errors

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,21 +10,23 @@ class Form extends Component {
       time: '',
       number: '',
     }
+  }
   
-
-  return (
-    <form className='resy-form'>
-      <label htmlFor='name'/>
-      <input type='input' className='inputs' name='name' id='name' placeholder='Name'/>
-      <label htmlFor='date'/>
-      <input type='date' className='inputs' name='date' id='date'/>
-      <label htmlFor='time'/>
-      <input type='time' className='inputs' name='time' id='time'/>
-      <label htmlFor='number'/>
-      <input type='input' className='inputs' name='number' id='number' placeholder='Number of guests'/>
-      <button type='submit' className='inputs' >Make Reservation</button>
-    </form>
-  )
+  render() {
+    return (
+      <form className='resy-form'>
+        <label htmlFor='name'/>
+        <input type='input' className='inputs' name='name' id='name' placeholder='Name'/>
+        <label htmlFor='date'/>
+        <input type='date' className='inputs' name='date' id='date'/>
+        <label htmlFor='time'/>
+        <input type='time' className='inputs' name='time' id='time'/>
+        <label htmlFor='number'/>
+        <input type='input' className='inputs' name='number' id='number' placeholder='Number of guests'/>
+        <button type='submit' className='inputs' >Make Reservation</button>
+      </form>
+    )
+  }
 }
 
 export default Form
